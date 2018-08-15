f9da4ff55f1e7b2444961e74bab0366939532b0c
DESKTOP-BORCB27\Deepak <deepak.deepak.g280@gmail.com>
2018-08-16 00:17:41 +0530

    First commit

---
+++ InternetConnectionCheckingSample/InternetConnectionCheckingSample/InternetConnectionCheckingSample/ViewModels/InternetConnectionStatusViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Plugin.Connectivity;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace InternetConnectionCheckingSample.ViewModels
+{
+    public class InternetConnectionStatusViewModel : INotifyPropertyChanged
+    {
+        public InternetConnectionStatusViewModel()
+        {
+            CheckNetworkOnStart();
+
+            NetworkContinousely();
+        }
+
+        private string _internetStatus;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs((propertyName)));
+        }
+
+        public string NetWorkStatus
+        {
+            get { return _internetStatus; }
+            set
+            { _internetStatus = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void CheckNetworkOnStart()
+        {
+            NetWorkStatus = CrossConnectivity.Current.IsConnected ? "Good Internet Connectios" : "Check Internet Connection";
+        }
+
+        private void NetworkContinousely()
+        {
+            CrossConnectivity.Current.ConnectivityChanged += (sender, args) =>
+            {
+                NetWorkStatus = args.IsConnected ? "Good Internet Connectios" : "Check Internet Connection";
+            };
+        }
+    }
+}
---
+++ InternetConnectionCheckingSample/InternetConnectionCheckingSample/InternetConnectionCheckingSample/Views/InternetConnectionStatusView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using InternetConnectionCheckingSample.ViewModels;
+using Xamarin.Forms.Xaml;
+
+namespace InternetConnectionCheckingSample.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InternetConnectionStatusView
+    {
+        public InternetConnectionStatusView()
+        {
+            InitializeComponent();
+
+            BindingContext = new InternetConnectionStatusViewModel();
+        }
+    }
+}
